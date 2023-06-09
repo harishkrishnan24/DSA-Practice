9c65db2b56535c4dcf8cbb5ab7b10d4bcc662a94
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-09 22:21:40 +0530

    trailing zero of a factorial

---
+++ mathematics/05-trailing_zeros_factorial.test.js
@@ -0,0 +1,25 @@
+// Time - O(logn) Space - O(1)
+
+function trailingZerofactorial(num) {
+  let result = 0;
+
+  for (let i = 5; i <= num; i = i * 5) {
+    result = result + Math.floor(num / i);
+  }
+
+  return result;
+}
+
+describe("trailingZerofactorial", () => {
+  test("trailingZerofactorial(4)", () => {
+    expect(trailingZerofactorial(5)).toBe(1);
+  });
+
+  test("trailingZerofactorial(6)", () => {
+    expect(trailingZerofactorial(10)).toBe(2);
+  });
+
+  test("trailingZerofactorial(0)", () => {
+    expect(trailingZerofactorial(100)).toBe(24);
+  });
+});
