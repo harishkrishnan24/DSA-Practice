27c0252827df0988567572c3d548c1c5bed571c5
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-06-16 11:08:49 +0530

    fix recursive code

--- mathematics/08-prime.test.js
+++ mathematics/08-prime.test.js
@@ -5,7 +5,7 @@ function isPrime(num) {
 
   if (num % 2 == 0 || num % 3 == 0) return false;
 
-  for (let i = 5; i * i < num; i = i + 6) {
+  for (let i = 5; i * i <= num; i = i + 6) {
     if (num % i == 0 || num % (i + 2) == 0) {
       return false;
     }
