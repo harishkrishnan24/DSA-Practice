319f5423d952a0009b1cf9186a96b71fe6287228
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-12-18 09:35:05 +0530

    print N to 1

---
+++ Recursion/01-print_N_to_1.js
@@ -0,0 +1,13 @@
+function printNto1(n) {
+  if (n === 0) return;
+
+  console.log(n);
+
+  printNto1(n - 1);
+}
+
+console.log(printNto1(5)); // 5 4 3 2 1
+console.log(printNto1(2)); // 2 1
+
+// Time - O(n)
+// Space - O(n)
