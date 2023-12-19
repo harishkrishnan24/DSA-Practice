df9fa8c01404ba5f39da3daf1215dc7f3acc7289
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-12-19 10:00:17 +0530

    bubble sort

---
+++ Sorting/01-bubble_sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let swapped = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+    if (!swapped) return arr;
+    swapped = false;
+  }
+
+  return arr;
+}
+
+console.log(bubbleSort([3, 2, 6, 10, 4])); // [2, 3, 4, 6, 10]
