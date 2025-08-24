61337111289713d30672412b22b56e3cd8d4706b
harishkrishnan24 <harishkrishnan1993@gmail.com>
2025-08-24 16:06:23 +0200

    [Math] Count digits

--- Mathematics/01_Count_Digits.js
+++ Mathematics/01_Count_Digits.js
@@ -1,6 +1,10 @@
 function countDigits(num) {
   let numOfDigits = 0;
 
+  if (num === 0) {
+    return 1;
+  }
+
   while (num > 0) {
     num = Math.floor(num / 10);
     numOfDigits++;
@@ -9,6 +13,8 @@ function countDigits(num) {
   return numOfDigits;
 }
 
-console.log(countDigits(9235)); // 4
-console.log(countDigits(38)); // 2
-console.log(countDigits(7)); // 1
+console.log(countDigits(7536)); // Expected: 4
+console.log(countDigits(758)); // Expected: 3
+console.log(countDigits(0)); // Expected: 1
+console.log(countDigits(123456789)); // Expected: 9
+console.log(countDigits(1)); // Expected: 1
